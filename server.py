c6a36f7d18e50697adcdd3686fc3fad5942eb56d
George Tudor <tudor.george002@gmail.com>
2024-09-19 20:01:59 +0300

    Added Templating and Navigation Bar

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Gika',
+        'title': 'Prima postare',
+        'content': 'Mesajul meu din prima postare',
+        'date_posted': '19 Septembrie 2024'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'A doua postare',
+        'content': 'Mesajul meu din a doua postare',
+        'date_posted': '20 Mai 2024'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About!')
 
 if __name__ == '__main__':
     app.run(debug=True)
