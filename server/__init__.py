55e47586abac8abdf98c644dd1c17be246b7d67c
George Tudor <tudor.george002@gmail.com>
2024-09-26 01:29:07 +0300

    Add custom error pages with blueprints. Very easy now

--- server/__init__.py
+++ server/__init__.py
@@ -29,9 +29,11 @@ def create_app(config_class=Config):
     from server.users.routes import users
     from server.posts.routes import posts
     from server.main.routes import main
+    from server.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
